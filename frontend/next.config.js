6fb67e1a92c18efc414c26f7ed988792cdae3dd0
Davinder9297 <davindergiri765@gmail.com>
2024-04-24 19:52:50 +0530

    updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 const nextConfig = {
   reactStrictMode: false,
+  ignoreBuildErrors: true,
   publicRuntimeConfig: {
     // Will be available on both server and client
     API_BASE_URL: process.env.API_BASE_URL
